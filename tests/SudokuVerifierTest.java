b50b01cc914c895b241d7312704a99f1fe8e0a01
Cheddar <157208341+CheduZ@users.noreply.github.com>
2024-11-26 15:25:58 +0200

    Task done

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,6 +10,14 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+
+String tooShort = "17369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String hasChar = "A17369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String invalidRow = "457369825632158947918724316825437169791586432346912758289643571573291684164875293";
+String invalidColumn = "457369821632158947918724356825437169791586432346912758289643571573291684164875293";
+String hasZero = "017369825632158947958724316825437169791586432346912758289643571573291684164875293";
+
+
 SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -22,8 +30,36 @@ SudokuVerifier v = new SudokuVerifier();
 	public void testIncorrectString() {
 		int a = v.verify(i);
 		assertEquals("incorrect string", a, -2);
-		
+	}
+
+	@Test
+	public void testCharString() {
+		int a = v.verify(hasChar);
+		assertEquals("Has characters", a, 1);
 	}
 	
+	@Test
+	public void testShortString() {
+		int a = v.verify(tooShort);
+		assertEquals("Not correct length", a, -1);
+	}
+	
+	@Test
+	public void testInvalidRowString() {
+		int a = v.verify(invalidRow);
+		assertEquals("Dublicate number on row", a, -3);
+	}
+	
+	@Test
+	public void testInvalidColumnString() {
+		int a = v.verify(invalidColumn);
+		assertEquals("Dublicate number on row", a, -4);
+	}
+	
+	@Test
+	public void testWithZeroString() {
+		int a = v.verify(hasZero);
+		assertEquals("Dublicate number on row", a, -1);
+	}
 	
 }
